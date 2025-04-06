9bf5afa20e1783a522ad7a663e33b6de492560cc
Petarpg <petarivvvvv@gmail.com>
2025-04-06 16:24:05 +0300

    Patch 1.13 - Building - PP
    Added:
    - Filter by category and subcategory
    - Login and register buttons

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,24 +9,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\SubCategoryRepository;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'products' => $productRepository->findBy([], ['id' => 'DESC']),
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 
     #[Route('/home/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
-    public function show(Product $product, ProductRepository $productRepository): Response
+    public function show(Product $product, ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
         $lastProducts = $productRepository->findBy([], ['id' => 'DESC'], 5);
         return $this->render('home/show.html.twig', [
             'product' => $product,
             'products' => $lastProducts,
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/home/product/subcategory/{id}/filter', name: 'app_home_product_filter', methods: ['GET'])]
+    public function filter($id, SubCategoryRepository $subCategoryRepository, CategoryRepository $categoryRepository): Response
+    {
+        $subCategory = $subCategoryRepository->find($id);
+        if (!$subCategory) {
+            throw $this->createNotFoundException('SubCategory not found');
+        }
+        return $this->render('home/filter.html.twig', [
+            'products' => $subCategory->getProducts(),
+            'subCategory' => $subCategory,
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }
